721ef5837dc38ad0d5a70114d9e479abe800c690
sywtang <samywtang@gmail.com>
2021-01-09 23:34:26 -0500

    Add header and search movies component

---
+++ frontend/src/components/Header/Header.js
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <header className="app-header">
+      <h1 className="main-heading">The Shoppies</h1>
+    </header>
+  );
+};
+
+export default Header;
---
+++ frontend/src/components/Search/Search.js
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react";
+
+const Search = ({ handleValue }) => {
+  const [text, setText] = useState("");
+
+  const handleChange = (event) => {
+    event.preventDefault();
+    setText(event.target.value);
+  };
+
+  useEffect(() => {
+    if (text === "" || text) handleValue(text);
+  }, [text, handleValue]);
+
+  return (
+    <form className="movie-form" onSubmit={(event) => event.preventDefault()}>
+      <label className="movie-form__title">Movie title</label>
+      <input
+        className="movie-form__input"
+        name="search"
+        type="text"
+        value={text}
+        onChange={(event) => handleChange(event)}
+      />
+    </form>
+  );
+};
+
+export default Search;
